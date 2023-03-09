fca5749ccdb287125f2b1d507853bd869ea91a60
Emmanuel Bolarinwa <gbotemibolarinwa@gmail.com>
2023-03-09 15:52:35 +0000

    new files

---
+++ dbt/models/staging/stg_2015_data.sql
@@ -0,0 +1,16 @@
+{{ config(materialized="view") }}
+
+select 
+    users,	
+    repo_name,
+    type,
+
+    cast(id	as integer) as id,
+    cast(public	as boolean) as public,
+    cast(created_at	as timestamp) as created_at,
+    cast(repo_id as integer) as repo_id,
+    cast(org_exists	as boolean) as org_exists,
+    cast(count_commits as integer) as count_commits	
+    
+from {{ source("staging", "2015") }}
+limit 10 
